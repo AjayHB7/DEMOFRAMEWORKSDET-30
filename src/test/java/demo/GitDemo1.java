e06bae9efc5153056cccbdd54404c803c0f46b7c
Chaitra9632 <chaitra.sreenivas88@gmail.com>
2022-03-11 11:35:30 +0530

    Chaitra second commit

--- src/test/java/demo/GitDemo1.java
+++ src/test/java/demo/GitDemo1.java
@@ -10,6 +10,9 @@ public class GitDemo1
 		System.out.println("this is git");
 		System.out.println("this is conflict");
 		System.out.println("this is second conflict");
+		
+		
+		System.out.println("This is another change");
 	}
 
 }
